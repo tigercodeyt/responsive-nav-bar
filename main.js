d186f1e54a0ea762351310191196bd03f80b6621
TIGER CODE YT <89652386+tigercodeyt@users.noreply.github.com>
2022-12-31 15:24:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav-list');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('active')
+    navList.classList.toggle('active')
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () =>{
+    hamburger.classList.remove('active')
+    navList.classList.remove('active')
+}))
